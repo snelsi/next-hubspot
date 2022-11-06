b8581e8639617827974e8118aaa07bed1be6a841
Roman Zhuravlov <snelsithepirate@gmail.com>
2022-11-06 22:21:19 +0200

    Migrate to @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
 const packageJson = require("./package.json");
@@ -20,13 +20,7 @@ const config = {
       sourcemap: true,
     },
   ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    terser(),
-  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
 };
 
 export default config;
